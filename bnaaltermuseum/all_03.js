0e5d6924f001687cfff1b3e88bfee5b087d8d10b
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-06 00:45:25 +0900

    map

---
+++ bnaaltermuseum/all_03.js
@@ -0,0 +1,102 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76842457873613, 35.00022238171323]
+            },
+            properties: {
+                title: 'アートが第一だから',
+                date: '2021-7-21 16:33',
+                address: 'BnA Alter Museum',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.77076930434038, 35.00364830848251]
+            },
+            properties: {
+                title: '僕は抱かれた側',
+                date: '2022-07-21 22:12',
+                address: '鳥貴族 四条河原町店',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76817085126663, 35.000149807595236]
+            },
+            properties: {
+                title: '高島屋でよかったですよね？',
+                date: '2022-07-24  10:07',
+                address: '河原町高辻交差点',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76746177768985, 35.0001341627423]
+            },
+            properties: {
+                title: 'お前はほんとのパパじゃない',
+                date: '2022-07-24  10:13',
+                address: 'KAMEE COFFEE KYOTO 高辻店',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.77087926820195, 34.997303330136546]
+            },
+            properties: {
+                title: '海苔じゃなくて苔なんです',
+                date: '2022-07-24  14:30頃',
+                address: 'Picaro Eis',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.7672035086636, 34.99773270800419]
+            },
+            properties: {
+                title: '女尊男卑',
+                date: '2022-07-25  12:10',
+                address: 'ＥＳＬＥＡＤ ＫＡＷＡＲＡＭＡＣＨＩ前',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76610542193487, 34.99538439351177]
+            },
+            properties: {
+                title: '4泊5日',
+                date: '2022-07-25  12:21',
+                address: 'ブーブーパーク',
+                link: 'none',
+                url: ''
+            }
+        }
+    ]
+};
---
+++ submit.php
@@ -0,0 +1,249 @@
+<?php
+
+mb_language("ja");
+mb_internal_encoding("UTF-8");
+
+// メッセージを保存するファイルのパス設定
+define( 'FILENAME', 'draft.csv');
+
+// 変数の初期化
+$page_flag = 0;
+
+if( !empty($_POST['btn_confirm']) ) {
+
+	$page_flag = 1;
+	session_start();
+	$_SESSION['page'] = true;
+
+} elseif( !empty($_POST['btn_submit']) ) {
+
+	if( $file_handle = fopen( FILENAME, "a") ) {
+
+		// 書き込むデータを作成
+		$data = "'".$_POST['things']."','".$_POST['essay']."','".$_POST['map']."','".$_POST['date']."','".$_POST['time']."'\n\n";
+
+		// 書き込み
+		fwrite( $file_handle, $data);
+
+		// ファイルを閉じる
+		fclose( $file_handle);
+	}
+
+	session_start();
+	if( !empty($_SESSION['page']) && $_SESSION['page'] === true ) {
+
+	$page_flag = 2;
+
+	// 変数とタイムゾーンを初期化
+	$header = null;
+	$auto_reply_subject = null;
+	$auto_reply_text = null;
+	$admin_reply_subject = null;
+	$admin_reply_text = null;
+	date_default_timezone_set('Asia/Tokyo');
+
+	// ヘッダー情報を設定
+	$header = "MIME-Version: 1.0\n";
+	$header .= "From: creative-community.space <pehu@creative-community.space>\n";
+	$header .= "Reply-To: creative-community.space <pehu@creative-community.space>\n";
+
+	// 件名を設定
+	$admin_reply_subject = 'Submit | things that i (we) heard';
+  
+	// 本文を設定
+	$admin_reply_text .= "\n" . $_POST['things'] . "\n";
+	$admin_reply_text .= "\n" . nl2br($_POST['essay']) . "\n";
+	$admin_reply_text .= "\n" . $_POST['map'] . "\n";
+	$admin_reply_text .= "" . $_POST['date'] .  " ";
+	$admin_reply_text .= " ". $_POST['time'] . "\n\n\n";
+	$admin_reply_text .= "https://creative-community.space/map/heard/";
+
+	mb_send_mail( 'pehu@creative-community.space', $admin_reply_subject, $admin_reply_text, $header);
+
+	} else {
+		$page_flag = 0;
+	}
+}
+?>
+    <!DOCTYPE html>
+    <html lang="ja">
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="icon" href="logo.png">
+        <link rel="stylesheet" href="form.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville:ital@0;1&display=swap" rel="stylesheet">
+        <title>Submit | things that i (we) heard</title>
+        <style type="text/css">
+            body {
+                background: #CDCBCC;
+                color: #fff;
+                margin: 0;
+            }
+            
+            #main u {
+                background: #fff;
+                color: #000;
+                text-decoration: none;
+            }
+            
+            #submit a {
+                color: #fff;
+                background: #CDCBCC !important;
+            }
+            
+            #submit sup {
+                padding:0 0.5rem;
+                font-size:75%;
+            }
+            
+            #submit a:hover {
+                color: #fff;
+                background: #000 !important;
+            }
+            
+            .content a,
+            #credit a {
+                color: #fff;
+            }
+            
+            .content a:hover,
+            #credit a:hover {
+                color: #000;
+                background: #fff !important;
+            }
+            
+            details summary {
+                color: #000;
+                background-color: #fff;
+            }
+            
+            details summary:hover {
+                background: #000;
+                color: #fff;
+            }
+            
+            details[open] summary:hover {
+                background: #CDCBCC;
+                color: #fff;
+            }
+            
+            body,
+            #credit,
+            #main input[type="text"],
+            #main input[type="name"],
+            #main input[type="url"],
+            #main input[type="email"],
+            #main input[type="date"],
+            #main input[type="time"],
+            #main input[type="submit"],
+            #main textarea {
+                font-family: 'Libre Baskerville', "YuMincho", "Yu Mincho", "游明朝体", serif;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div id="credit">
+          <a href="#" onClick="history.back(); return false;" target="_parent"></a>
+          <a href="#" onClick="history.back(); return false;" target="_parent">↲</a>
+        </div>
+
+        <div id="submit">
+            <?php if( $page_flag === 1 ): ?>
+            <section id="main" class="form">
+                <form method="post">
+                    <div id="post">
+                        <div>
+                            <div>
+                                <div class="submit">
+                                    <h1><u>聞いた言葉</u><br/>
+                                        <?php echo $_POST['things']; ?>
+                                    </h1>
+                                    <p><u>聞いた言葉・場所にまつわるエピソード</u></p>
+                                    <p>
+                                        <?php echo nl2br($_POST['essay']); ?>
+                                    </p>
+                                </div>
+                                <hr/>
+                                <h1>
+                                    <u>言葉を聞いた場所</u>
+                                    <a href="<?php echo $_POST['map']; ?>" target="_blank"><?php echo $_POST['map']; ?></a>
+                                </h1>
+                                <h1>
+                                    <u>言葉を聞いた日時</u><br/>
+                                    <?php echo $_POST['date']; ?>
+                                    <?php echo $_POST['time']; ?>
+                                </h1>
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                    <p id="next">
+                        <input type="submit" name="btn_submit" value="投稿する">
+                        <input type="submit" value="修正する" onClick="history.back(); return false;">
+                    </p>
+
+                    <input type="hidden" name="things" value="<?php echo $_POST['things']; ?>">
+                    <input type="hidden" name="essay" value="<?php echo $_POST['essay']; ?>">
+                    <input type="hidden" name="map" value="<?php echo $_POST['map']; ?>">
+                    <input type="hidden" name="date" value="<?php echo $_POST['date']; ?>">
+                    <input type="hidden" name="time" value="<?php echo $_POST['time']; ?>">
+
+
+                </form>
+            </section>
+            <?php elseif( $page_flag === 2 ): ?>
+
+            <div class="thankyou">
+                <h1>ご投稿ありがとうございました。</h1>
+                <p>近日中に、あなたが投稿した言葉をデジタル地図に追加します。</p>
+                <h1><a href="/map/heard/">things that i (we) heard</a></h1>
+            </div>
+
+            <?php else: ?>
+
+            <footer>
+              <hr/>
+                <p>令和四年版　夏の自由研究</p>
+                <h1><i>things that i (we) heard<br/>around BnA Alter Museum</i></h1>
+                <p>ビーエヌエーオルターミュージアムの周りで聞いた言葉</p>
+            </footer>
+            <section id="main" class="form">
+                <form action="" method="post">
+                    <div class="submit">
+                        <h1><u>あなたが聞いた言葉</u>
+                            <sup>※ 必須</sup>
+                            <br/>
+                            <input type="text" name="things" value="<?php if( !empty($_POST['things']) ){ echo $_POST['things']; } ?>" placeholder="things that you heard" required>
+                        </h1>
+                        <h1><u>聞いた言葉・場所にまつわるエピソード</u></h1>
+                        <p><textarea name="essay" rows="12.5" placeholder="About the things that you heard"><?php if( !empty($_POST['essay']) ){ echo $_POST['essay']; } ?></textarea></p>
+                        <hr/>
+                        <h1><u>言葉を聞いた場所</u>
+                            <sup>※ 必須</sup>
+                            <br/>
+                            <input type="url" name="map" value="<?php if( !empty($_POST['map']) ){ echo $_POST['map']; } ?>" placeholder="ex https://goo.gl/maps/〇〇" required><br/></h1>
+                            <p>
+                              <a href="https://www.google.co.jp/maps/" target="_blank">Google Map</a>
+                              で言葉を聞いた場所を探し、上記のフォームにリンクを添付してください</p>
+                            <hr/>
+                        <h1><u>言葉を聞いた日時</u>
+                            <sup>※ 必須</sup>
+                            <br/>
+                            <input type="date" name="date" value="<?php if( !empty($_POST['date']) ){ echo $_POST['date']; } ?>" required>
+                            <input type="time" name="time" value="<?php if( !empty($_POST['time']) ){ echo $_POST['time']; } ?>" required><br/></h1>
+                        <p><input type="submit" name="btn_confirm" value="確認する"></p>
+                    </div>
+                </form>
+            </section>
+            <?php endif; ?>
+        </div>
+    </body>
+
+    </html>
