3abaa8babc62e73bcab4696bad866272128d3654
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-13 23:31:36 +0900

    all_05.js

---
+++ bnaaltermuseum/all_05.js
@@ -0,0 +1,196 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76842457873613, 35.00022238171323]
+            },
+            properties: {
+                title: 'アートが第一だから',
+                date: '2021-7-21 16:33',
+                address: 'BnA Alter Museum'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.77076930434038, 35.00364830848251]
+            },
+            properties: {
+                title: '僕は抱かれた側',
+                date: '2022-07-21 22:12',
+                address: '鳥貴族 四条河原町店'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76817085126663, 35.000149807595236]
+            },
+            properties: {
+                title: '高島屋でよかったですよね？',
+                date: '2022-07-24  10:07',
+                address: '河原町高辻交差点'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76746177768985, 35.0001341627423]
+            },
+            properties: {
+                title: 'お前はほんとのパパじゃない',
+                date: '2022-07-24  10:13',
+                address: 'KAMEE COFFEE KYOTO 高辻店'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.77087926820195, 34.997303330136546]
+            },
+            properties: {
+                title: '海苔じゃなくて苔なんです',
+                date: '2022-07-24  14:30頃',
+                address: 'Picaro Eis'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.7672035086636, 34.99773270800419]
+            },
+            properties: {
+                title: '女尊男卑',
+                date: '2022-07-25  12:10',
+                address: 'ＥＳＬＥＡＤ ＫＡＷＡＲＡＭＡＣＨＩ前'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76610542193487, 34.99538439351177]
+            },
+            properties: {
+                title: '4泊5日',
+                date: '2022-07-25  12:21',
+                address: 'ブーブーパーク'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76788133860566,35.000006276002495]
+            },
+            properties: {
+                title: '麦茶はいいんやで、ミネラルが入ってる',
+                date: '2022-08-13  09:49',
+                address: 'セブン-イレブン 京都河原町高辻店'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76828078144308,35.00375576142723]
+            },
+            properties: {
+                title: 'It is in a house? NO',
+                date: '2022-08-13  10:25',
+                address: '京都河原町駅'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.77182001546186,34.99863256550397]
+            },
+            properties: {
+                title: 'ご入用の方お持ち帰りください',
+                date: '2022-08-13  17:07',
+                address: '川魚 のと正'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.7681257561544,34.99899075302402]
+            },
+            properties: {
+                title: 'そんなとこ止まらんときや～',
+                date: '2022-08-13  17:12',
+                address: '京都市下京区松川町'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.7695921021695,34.995663687378766]
+            },
+            properties: {
+                title: 'ちょっと一瞬だけ待ってて',
+                date: '2022-08-13  20:31',
+                address: '京都市東山区東橋詰町'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.7681689800413,34.99911706223267]
+            },
+            properties: {
+                title: 'ソルト？',
+                date: '2022-08-13  21:02',
+                address: '京都市下京区清水町'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76326297145846,34.99999864332054]
+            },
+            properties: {
+                title: '今日1日ありがとうございました',
+                date: '2022-08-13  20:26',
+                address: 'チカキッサ.タブコ'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.75806632674124,35.00542582767942]
+            },
+            properties: {
+                title: '一応3日間ちょこちょこやろうかなと思うけど、オンデマンドで',
+                date: '2022-08-13  20:28',
+                address: '京都芸術センター'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76685243417177,34.99898418379705]
+            },
+            properties: {
+                title: 'ちょっと待ってここでちょっと調べるわ',
+                date: '2022-08-13  22:38',
+                address: '鉄板とお酒 宗や'
+            }
+        }
+    ]
+};
