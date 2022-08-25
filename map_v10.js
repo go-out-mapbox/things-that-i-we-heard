0e4eda2c4f0bc32305f3a9acd6e8719bfd227f54
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-26 04:16:06 +0900

    map

---
+++ map_v10.js
@@ -0,0 +1,593 @@
+const geojson = {
+    type: 'FeatureCollection',
+    features: [{
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4738151725678, 34.62495140884785]
+            },
+            properties: {
+                title: '温めましょうね',
+                date: '2021年11月15日 16:40',
+                address: '住之江区北加賀屋５丁目４−７０',
+                link: '',
+                url: 'https://newlifecollection.com/ca100/317/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4768335595408, 34.621277963475166]
+            },
+            properties: {
+                title: 'ほらいた！お前ちゃんと作れんのか？',
+                date: '2022年2月2日 15:40',
+                address: '住之江区北加賀屋２丁目１１−８',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49139840617704, 34.61760551293973]
+            },
+            properties: {
+                title: '冬もの、売っちゃいます！',
+                date: '2022年2月24日 20:35',
+                address: '住吉区東粉浜３丁目２３−２３',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49070608679915, 34.6153494851478]
+            },
+            properties: {
+                title: '熟女いけてもうたら幅広がるよな',
+                date: '2022年3月2日 23:57',
+                address: '住之江区粉浜３丁目９−２４',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46463988902042, 34.6063333988492]
+            },
+            properties: {
+                title: 'でっか！なんやねんこの白菜',
+                date: '2022年2月8日 18:50',
+                address: '住之江区新北島６丁目１−５５',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49063227353213, 34.615485710748246]
+            },
+            properties: {
+                title: 'オダギリジョーに似てるって言われませんか？',
+                date: '2022年2月6日 16:30',
+                address: '住之江区粉浜３丁目８−７',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49010563251517, 34.61261492920843]
+            },
+            properties: {
+                title: '東京のもんには敵わんけ',
+                date: '2022年2月9日 18:00',
+                address: '住吉区長峡町３',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4874222705268, 34.615542730420984]
+            },
+            properties: {
+                title: 'ここは客の質がいい。味もいいし高くないやろ？',
+                date: '2022年1月23日 20:20',
+                address: '住之江区粉浜西３丁目１',
+                link: '',
+                url: 'https://newlifecollection.com/ca100/322/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46533366916475, 34.60792378090896]
+            },
+            properties: {
+                title: '鬼滅鬼滅鬼滅欲しいー！',
+                date: '2022年2月18日 14:31',
+                address: '住之江区新北島５丁目２−１８',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47653184582225, 34.62018128704868]
+            },
+            properties: {
+                title: 'ヤマガタさんが買ってきてくれるような煎餅やね',
+                date: '2022年2月10日 12:33',
+                address: '住之江区西加賀屋１丁目２−３４',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.479546430041, 34.622406299630654]
+            },
+            properties: {
+                title: '振り返ってもらいま〜す',
+                date: '2022年2月10日 11:57',
+                address: '住之江区北加賀屋２丁目５−２６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48208837084155, 34.64567230644259]
+            },
+            properties: {
+                title: 'うわー！あー！おー！',
+                date: '2022年2月12日 13:00',
+                address: '西成区津守１丁目１３',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48242649189115, 34.64768126307645]
+            },
+            properties: {
+                title: '祝全国大会出場放送部',
+                date: '2022年2月12日 13:01',
+                address: '西成区津守１丁目１３−１０',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47929044106075, 34.64965759003991]
+            },
+            properties: {
+                title: 'OK出船どうぞ',
+                date: '2022年2月12日 13:18',
+                address: '西成区北津守３丁目１２−１',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4742669195051, 34.64796228767946]
+            },
+            properties: {
+                title: 'それはそれでダメかもよ',
+                date: '2022年2月12日 13:31',
+                address: '大正区千島２丁目',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4722662854645, 34.64851351816951]
+            },
+            properties: {
+                title: '顔あげてアイコンタクト取ったら間狙える。間絞れ',
+                date: '2022年2月12日 13:33',
+                address: '大正区千島２丁目７',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47155928370017, 34.650247615171935]
+            },
+            properties: {
+                title: 'この辺バスないとヤバいから',
+                date: '2022年2月12日 13:37',
+                address: '大正区千島３丁目１１−８',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46719338298726, 34.64887278734386]
+            },
+            properties: {
+                title: '何医者でも空いてる時ありますねん',
+                date: '2022年2月12日 13:42',
+                address: '大正区北村３丁目２',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.46405317361564, 34.64963348785348]
+            },
+            properties: {
+                title: 'おー素晴らしい',
+                date: '2022年2月12日 13:47',
+                address: '大正区北村３丁目３−７０',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.45957347295547, 34.64791069012345]
+            },
+            properties: {
+                title: '船が揺れます。足元にご注意ください',
+                date: '2022年2月12日 14:02',
+                address: '大正区北恩加島２丁目５−２５',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.45472036918443, 34.642937455394105]
+            },
+            properties: {
+                title: 'そう真っ直ぐ！いけてるいけてる',
+                date: '2022年2月12日 14:15',
+                address: '大正区鶴町３丁目１５−４',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47114983740863, 34.646430371239646]
+            },
+            properties: {
+                title: '独特の形してるから色々変えるのは難しい',
+                date: '2022年2月12日 15:30',
+                address: '大正区小林東３丁目５−２０',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4747163811283, 34.64571182089179]
+            },
+            properties: {
+                title: '小川に生き物を持ち込まないでね',
+                date: '2022年2月12日 15:39',
+                address: '大正区小林東２丁目５−５９',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47450268250037, 34.64320180855712]
+            },
+            properties: {
+                title: '「二番手になるのはダサいよな」<br/>「黒歴史です」',
+                date: '2022年2月12日 15:42',
+                address: '大正区小林東２丁目６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.47845476535912, 34.643536301603376]
+            },
+            properties: {
+                title: 'お待たせしました。',
+                date: '2022年2月12日 15:59',
+                address: '大正区平尾１丁目１−２６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49301780969517, 34.62983814426073]
+            },
+            properties: {
+                title: 'あれ夏場だけ。プール',
+                date: '2022年2月12日 17:42',
+                address: '西成区岸里３丁目９−９',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4902407781745, 34.62404741860256]
+            },
+            properties: {
+                title: 'しょんぼりせんといて。悔しいん？',
+                date: '2022年2月12日 18:28',
+                address: '西成区玉出西２丁目６−６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4815854725598, 34.616730040619274]
+            },
+            properties: {
+                title: 'あんたステーキ嫌いやろ？',
+                date: '2022年2月17日 11:50',
+                address: '住之江区中加賀屋３丁目１',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48638856608255, 34.615822882690466]
+            },
+            properties: {
+                title: 'ここで一つ、面白い話をします。',
+                date: '2022年3月19日 14:30',
+                address: '住之江区粉浜西３丁目１−５',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48590991829985, 34.61701733866016]
+            },
+            properties: {
+                title: 'ピヨーンとおいでぇ',
+                date: '2022年3月14日 20:30',
+                address: '住之江区粉浜西３丁目１−７',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48590991829985, 34.61701733866016]
+            },
+            properties: {
+                title: 'ヘイくっさマ〜ン',
+                date: '2022年3月16日 21:16',
+                address: '住之江区粉浜西３丁目１−６',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48816560208678, 34.61181334220366]
+            },
+            properties: {
+                title: '「森のクマさんやんな」<br/>「そうそうそう」',
+                date: '2022年5月22日 17:40',
+                address: '住之江区粉浜西３丁目１−６',
+                link: '',
+                url: 'https://newlifecollection.com/ca100/197/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48622009889897, 34.613230002839146]
+            },
+            properties: {
+                title: '新しい病院やんな。第一号や。',
+                date: '2022年6月5日 13:27',
+                address: '住之江区浜口西１丁目２−１３',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.4747314510672, 34.60904437510054]
+            },
+            properties: {
+                title: 'やっベーぞって知らん？',
+                date: '2022年6月5日 18:05',
+                address: '住之江区南加賀屋１丁目１',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.48973519007808, 34.62307593811133]
+            },
+            properties: {
+                title: 'プシュ-ッ<br/>（私の横を通り過ぎた直後、銃のように手を真っ直ぐ構え、撃った）',
+                date: '2022年6月6日 9:03',
+                address: '住之江区粉浜西１丁目１',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.41296124791904, 34.63679087370123]
+            },
+            properties: {
+                title: 'どうしたらいいんかなって考える',
+                date: '2022年6月8日 21:15頃',
+                address: '住之江区南港北２丁目１−１０',
+                link: '',
+                url: 'https://newlifecollection.com/ca97/1312/p-r97-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.50183216544207, 34.61987240976515]
+            },
+            properties: {
+                title: '優しいドーベルマンやったね',
+                date: '2022年6月19日 18:31',
+                address: '住吉区万代３丁目７',
+                link: '',
+                url: 'https://newlifecollection.com/ca98/1311/p-r-s/'
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.51847321820424, 34.617464998822186]
+            },
+            properties: {
+                title: '次の日学校やったら10時半には寝る',
+                date: '2022年6月19日 21:35',
+                address: '東住吉区山坂５丁目４−１３',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.49563793774598,34.700593520446134]
+            },
+            properties: {
+                title: 'IMAXって言ったらクリストファーノーラン',
+                date: '2022年6月28日 16:45',
+                address: '北区梅田３丁目 大阪駅前地下街 8号',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.76854055131543,35.00017558944718]
+            },
+            properties: {
+                title: 'なんか今めちゃくちゃアートっぽい',
+                date: '2022年7月23日 14:54',
+                address: '京都市下京区天満町２６７−１',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.75806632674124,35.00542582767942]
+            },
+            properties: {
+                title: '一応3日間ちょこちょこやろうかなと思うけど、オンデマンドで',
+                date: '2022年8月13日 20:28',
+                address: '京都市中京区山伏山町５５３',
+                link: 'none',
+                url: ''
+            }
+        },
+        {
+            type: 'Feature',
+            geometry: {
+                type: 'Point',
+                coordinates: [135.7445633422256,35.036892335892716]
+            },
+            properties: {
+                title: '人生一度きりやし、来年見とけよ',
+                date: '2022年8月13日 21:45',
+                address: '京都市北区紫野南舟岡町８２−１',
+                link: 'none',
+                url: ''
+            }
+        }
+
+    ]
+};
