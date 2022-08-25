4c14b669f7f64d9590e63d193f3f49fa657b81f0
∧° ┐ <we.are.pe.hu@gmail.com>
2022-08-26 05:48:40 +0900

    Update submit.php

--- submit.php
+++ submit.php
@@ -251,7 +251,7 @@ if( !empty($_POST['btn_confirm']) ) {
             <?php endif; ?>
         </div>
         <p class="link">
-            <a href="#" onclick="window.history.back(); return false;">↩︎</a>
+            <a href="/map/heard/" target="_parent>↩︎</a>
         </p>
     </body>
 
